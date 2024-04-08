ec6d98482d68dfb0604898dc6eee7b0e503cafe6
Rushi N Lukka <134401284+rushilukka@users.noreply.github.com>
2024-04-08 16:58:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from'./cmp/loginpg';
+import { Button, Form, Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+     <Container >
+    <Row className="my-5 ">
+      
+    <Login/>
+      </Row>
+    </Container>
+    </>
   );
 }
 
